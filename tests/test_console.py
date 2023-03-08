03a4fa8ce90990de6b52b2a3f100be1e804705cf
EthanWakeford <ethanwakeford@gmail.com>
2023-03-07 21:03:17 -0600

    added test console

---
+++ tests/test_console.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""test for console """
+from tests.test_models.test_base_model import test_basemodel
+from models.amenity import Amenity
+
+
+class test_console(test_basemodel):
+    """test for console"""
+
+    def test_name2(self):
+        """tests"""
+        pass
